a239257d904a7f92566e738d8db9a5f971a4b9cc
CIANDT\hamaraisa <hamaraisa@ciandt.com>
2022-06-13 10:03:18 -0300

    Java com Banco de Dados - Criando tabelas e manipulando dados com JDBC

---
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -0,0 +1,39 @@
+public class Cliente {
+
+    private String nome;
+    private Integer idade;
+    private String cpf;
+    private String rg;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
